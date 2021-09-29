f3d26d28058a101779e4423b9348c6ac90885350
vlasnata80@gmail.com <vlasnata80@gmail.com>
2021-09-29 21:02:51 +0300

    start

---
+++ Example_OOP/Iron.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Example_OOP
+{
+    class Iron
+    {
+        string _mark;
+        int _temp;
+        DateTime _date;
+        bool _isSteam;
+        bool _isOn;
+
+
+
+        public Iron(string mark,int temp,DateTime date)
+        {
+            _mark = mark;
+            _temp = temp;
+            _date = date;
+        }
+
+        public Iron()
+        {
+            _mark = "Scarlett";
+            _temp = 150;
+            _isOn = true;
+            _isSteam = true;
+            _date = new DateTime(2020, 05, 03);
+
+        }
+
+        public void SetPower(string onPower)
+        {
+            if (onPower == "On")
+                _isOn = true;
+            else _isOn = false;
+        }
+              
+
+        public void SetSteam(string action)
+        {
+            if (action == "Yes")
+                _isSteam = true;
+            else _isSteam = false;
+        }
+        public void SetTemp()
+        {
+            _temp += 20;
+        }
+
+        public int GetTemp()
+        {
+            return _temp;
+        }
+
+        public void Print()
+        {
+            Console.WriteLine($"Now you use {_mark}  with temp {_temp}" +
+                $".Buying date:{_date.ToShortDateString()}." +
+                $"Condition:{(_isOn==true?"On":"Off")} and steam :{(_isSteam == true ? "On" : "Off")}");
+        }
+    }
+}
---
+++ Example_OOP/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Example_OOP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           //First iron.
+            Iron iron = new Iron();
+            iron.Print();
+            string steam = "No";
+            iron.SetSteam(steam);
+            iron.Print();
+
+
+            //Second iron.
+            Console.WriteLine("Second iron:");
+            Iron iron1 = new Iron("Sansung", 150, new DateTime(2021, 10, 10));
+            iron1.Print();
+
+            //Change condidtional of iron(set "On")
+            string power = "On";
+            iron1.SetPower(power);
+            iron1.Print();
+            iron1.SetTemp();
+            iron1.Print();
+
+            Console.WriteLine($"{iron1.GetTemp()}");
+        }
+    }
+}
